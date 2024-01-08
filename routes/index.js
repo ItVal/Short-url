36c0f2d8f9947f35f4b13376b8a8c28b06fa2443
ItVal <valentinhonvj1@gmail.com>
2024-01-08 23:08:07 +0200

    feat create get route and controller

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,21 @@
 import express from 'express';
 const router = express.Router();
 
+import Url from '../models/url.js';
+
+//route Get /:code
+// desc Redirect to long/original Url
+router.get('/:code', async (req, res) => {
+	try {
+		const url = await Url.findOne({ urlCode: req.params.code });
+		if (url) {
+			return res.redirect(url.longUrl);
+		} else {
+			return res.status(404).json('no url found');
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json('Internal Server Error');
+	}
+});
 export default router;
--- routes/url.js
+++ routes/url.js
@@ -11,7 +11,7 @@ router.post('/shorten', async (req, res) => {
 	const { longUrl } = req.body;
 	const baseUrl = config.get('baseUrl');
 	//check base url
-	if (!validUrl.isUrl(baseUrl)) {
+	if (!validUrl.isUri(baseUrl)) {
 		return res.status(401).json('invalid url');
 	}
 
@@ -19,30 +19,31 @@ router.post('/shorten', async (req, res) => {
 	const urlCode = shortid.generate();
 
 	//check long url
-	if (validUrl.isUrl(longUrl)) {
-		try {
-			let url = await Url.findOne({ longUrl });
-			if (url) {
-				res.json(url);
-			} else {
-				const shortUrl = baseUrl + '/' + urlCode;
-				url = new Url({
-					longUrl,
-					shortUrl,
-					urlCode,
-					date: new Date(),
-				});
+	// if (validUrl.isUri(longUrl)) {
+	try {
+		let url = await Url.findOne({ longUrl });
+		if (url) {
+			res.json(url);
+		} else {
+			const shortUrl = baseUrl + '/' + urlCode;
+			url = new Url({
+				longUrl,
+				shortUrl,
+				urlCode,
+				date: new Date(),
+			});
 
-				await url.save();
-				res.json(url);
-			}
-		} catch (err) {
-			console.log(err);
-			res.status(500).json('Server error');
+			await url.save();
+			res.json(url);
 		}
-	} else {
-		res.status(401).json('Invalid long url');
+	} catch (err) {
+		console.log(err);
+		res.status(500).json('Server error');
 	}
+	// }
+	//  else {
+	// 	res.status(401).json('Invalid long url');
+	// }
 });
 
 export default router;
